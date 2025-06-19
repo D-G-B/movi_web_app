95ce2def4c115ee3c4519a08f535d787b374556b
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2025-06-19 17:58:51 +0200

    updating user html template

--- app.py
+++ app.py
@@ -16,5 +16,29 @@ with app.app_context():
 def home():
     return "Hello World!"
 
+@app.route('/users')
+def list_users():
+    pass
+
+@app.route('/users/<user_id>')
+def users_movies():
+    pass
+
+@app.route('/add_user')
+def add_user():
+    pass
+
+@app.route('/users/<user_id>/add_movie')
+def add_movie_to_user():
+    pass
+
+@app.route('/users/<user_id>/update_movie/<movie_id>')
+def update_movie():
+    pass
+
+@app.route('/users/<user_id>/delete_movie/<movie_id>')
+def delete_movie():
+    pass
+
 if __name__ == '__main__':
     app.run(debug=True)
